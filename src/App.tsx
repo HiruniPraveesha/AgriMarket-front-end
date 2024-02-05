63c5bfdd024a9698ffc027face4965880816728d
ThiliniWasana <wthilini418@gmail.com>
2024-02-05 15:59:23 +0530

    edited selectlogin

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import SignIn from "./pages/Login/SignIn";
 import SignUpBuyer from "./pages/Login/SignUpBuyer";
+import SelectLogin from "./pages/Login/selectLogin";
 
 const App: React.FC = () => {
   return (
     <div>
-      <SignUpBuyer />
-      <SignIn />
+      <SelectLogin />
     </div>
   );
 };
--- src/pages/Login/selectLogin.tsx
+++ src/pages/Login/selectLogin.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import backgroundImage from "../../assets/buyerSeller.png";
 
-const selectLogin: React.FC = () => {
-  return <div>SignUp-buyer:React.FC</div>;
+const SelectLogin: React.FC = () => {
+  return (
+    <div
+      className="container d-flex flex-column align-items-center justify-content-center vh-100"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover", // Adjust as needed
+        backgroundPosition: "center", // Center the background
+      }}
+    >
+      <div className="mb-4 fs-2 text-white">ARE YOU A BUYER OR SELLER ?</div>
+      <div className="d-flex flex-column flex-md-row my-5">
+        <button
+          type="button"
+          className="btn btn-buyer mb-3 mb-md-0 me-md-2 py-4 px-5 mx-5"
+          style={{
+            backgroundColor: "#DFFFC0",
+            borderRadius: "55px",
+            color: "#00BA29",
+            fontSize: "30px",
+          }}
+        >
+          BUYER
+        </button>
+        <button
+          type="button"
+          className="btn btn-seller py-4 px-5 mx-5 "
+          style={{
+            backgroundColor: "#DFFFC0",
+            borderRadius: "55px",
+            color: "#00BA29",
+            fontSize: "30px",
+          }}
+        >
+          SELLER
+        </button>
+      </div>
+    </div>
+  );
 };
 
-export default selectLogin;
+export default SelectLogin;
