7e82d87d44d99d2f78cb7a0de1fef604895e8b74
Hiruni Praveesha <126430052+HiruniPraveesha@users.noreply.github.com>
2024-02-06 14:13:27 +0530

    Merged SignIn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import SignIn from "./pages/Login/SignIn";
 import SignUpBuyer from "./pages/Login/SignUpBuyer";
 
 const App: React.FC = () => {
   return (
     <div>
       <SignUpBuyer />
+      <SignIn />
     </div>
   );
 };
