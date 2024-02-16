4a9f1486e6561c812b674f8fef16a51a8a04f2e0
PunsaraniJayawardhana <punsaranijayawardhana@gmail.com>
2024-02-16 20:43:03 +0530

    NewHomepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import SignIn from "./pages/Login/SignIn";
-import SignUpBuyer from "./pages/Login/SignUpBuyer";
-import SelectLogin from "./pages/Login/selectLogin";
-import MainHeader from "./components/Header-main";
-import MainFooter from "./components/Footer-main";
-import BecomeASeller from "./pages/BecomeASeller";
-import ProductMap from "./pages/ProductMap";
+import Homepage from "./pages/Homepage";
 
 const App: React.FC = () => {
   return (
     <div>
-      <MainHeader />
-      <div style={{ paddingBottom: "10%" }}></div>
-      <BecomeASeller />
-      <MainFooter />
+      
+      <Homepage />
     </div>
   );
 };
